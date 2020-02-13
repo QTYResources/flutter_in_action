07b84a3e85296379aac10dad1187ffe76bc051e6
xiaotuan <583168425@qq.com>
2020-02-13 12:49:49 +0800

    添加第11章=>block_download->实例：Http分块下载

---
+++ Chapter11/block_download/lib/main.dart
@@ -0,0 +1,147 @@
+import 'dart:io';
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState()  => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  void downloadHBuilder() async {
+    var url = "https://download.dcloud.net.cn/HBuilder.9.0.2.macosx_64.dmg";
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    print("dir: $dir");
+    var savePath = "$dir/HBuilder.9.0.2.macosx_64.dmg";
+    var file = File(savePath);
+    file.create();
+    await downloadWithChunks(url, savePath, onReceiveProgress: (received, total) {
+      if (total != -1) {
+        print("${(received / total * 100).floor()}%");
+      }
+      if (received == total) {
+        var file = File(savePath);
+        file.length().then((value) {
+          print("length: $value");
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("下载HBuilder"),
+              onPressed: () {
+                downloadHBuilder();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Downloading by spiting as file in chunks
+Future downloadWithChunks(
+  url,
+  savePath, {
+    ProgressCallback onReceiveProgress,
+}) async {
+  const firstChunkSize = 102;
+  const maxChunk = 3;
+
+  int total = 0;
+  var dio = Dio();
+  var progress = <int>[];
+
+  createCallback(no) {
+    return (int received, _) {
+      progress[no] = received;
+      if (onReceiveProgress != null && total != 0) {
+        onReceiveProgress(progress.reduce((a, b) => a + b), total);
+      }
+    };
+  }
+
+  Future<Response> downloadChunk(url, start, end, no) async {
+    progress.add(0);
+    --end;
+    return dio.download(
+      url,
+      savePath + "temp$no",
+      onReceiveProgress: createCallback(no),
+      options: Options(
+        headers: {"range": "bytes=$start-$end"},
+      ),
+    );
+  }
+
+  Future mergeTempFiles(chunk) async {
+    File f = File(savePath + "temp0");
+    IOSink ioSink = f.openWrite(mode: FileMode.writeOnlyAppend);
+    for (int i = 1; i < chunk; ++i) {
+      File _f = File(savePath + "temp$i");
+      await ioSink.addStream(_f.openRead());
+      await _f.delete();
+    }
+    await ioSink.close();
+    await f.rename(savePath);
+  }
+
+  Response response = await downloadChunk(url, 0, firstChunkSize, 0);
+  if (response.statusCode == 206) {
+    total = int.parse(response.headers.value(HttpHeaders.contentRangeHeader).split("/").last);
+    int reserved = total - int.parse(response.headers.value(HttpHeaders.contentLengthHeader));
+    int chunk = (reserved / firstChunkSize).ceil() + 1;
+    if (chunk > 1) {
+      int chunkSize = firstChunkSize;
+      if (chunk > maxChunk + 1) {
+        chunk = maxChunk + 1;
+        chunkSize = (reserved / maxChunk).ceil();
+      }
+      var futures = <Future>[];
+      for (int i = 0; i < maxChunk; ++i) {
+        int start = firstChunkSize + i * chunkSize;
+        futures.add(downloadChunk(url, start, start + chunkSize, i + 1));
+      }
+      await Future.wait(futures);
+    }
+    await mergeTempFiles(chunk);
+  }
+
+}
--- README.txt
+++ README.txt
@@ -73,3 +73,5 @@ Chapter11   文件操作与网络请求
     01. file_operation                                              ===> 文件操作
     02. http_request                                                ===> 通过HttpClient发起HTTP请求
     03. dio_http                                                    ===> Http请求-Dio http库
+    04. block_download                                              ===> 实例：Http分块下载
+    
